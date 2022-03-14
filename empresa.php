9081ecae7f6db0723e54437b9efdde002cf73f63
LeonardoDuarteBonafe <32874289+LeonardoDuarteBonafe@users.noreply.github.com>
2022-03-14 08:39:47 -0300

    versão 1.0 ivory

---
+++ empresa.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="style.css" rel="stylesheet">
+    <script type="text/javascript" src="main.js"></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+
+    <script src="https://smtpjs.com/v3/smtp.js"></script>
+
+    <link rel="icon" href="Images/Site-Icon/site-icon.webp">
+    <title>Margil Granitos</title>
+
+    <body class="page-empresa">
+        <?php
+        include 'php/navbar.html';
+        include 'php/whatsapp.html';
+        ?>
+
+        <div class="empresa">
+            <h2 class="title">
+                EMPRESA
+            </h2>
+            <div class="info">
+                <div class="text">
+                    <h2>Margil</h2> 
+                    <p>
+                        A MARGIL está presente no mercado desde 1987, localizada no pólo industrial do Mármore e Granito no Sul do Espírito Santo, localizada em Atílio Vivácqua, com a missão de atender as necessidades do mercado e de seus clientes, garantindo o mais alto padrão de qualidade e atendimento. Talento e competência se tornaram características que destacam o trabalho da empresa, atestado pelos seus clientes criando uma sólida relação de parceria, amizade e fidelidade. Com a história de responsabilidade, honestidade e competência nos produtos entregues, a confiança do mercado se fortaleceu.
+                        <br>
+                        Com uma equipe qualificada, a MARGIL tem se destacado também no setor pela garantia de entrega, produtos dentro das especificações combinadas, projetos personalizados e com os melhores acabamentos para garantir a estética e durabilidade dos materiais entregues.
+                        <br>
+                        Uma empresa completa, ofertando Chapas Brutas, Chapas Polidas e recortadas, atendendo desde as pequenas obras, até as grandes demandas.
+                        <br>
+                        Faça-nos uma visita!
+
+                    </p>
+                </div>
+                <div class="images">
+                    <div class="grande">
+                        <div id="1">
+                            <img src="Images/Empresa/Galeria/1.webp">
+                        </div>
+                    </div>
+                    <div class="pequena">
+                        <div id="2">
+                            <img src="Images/Empresa/Galeria/2.webp">
+                        </div>
+                        <div id="3">
+                            <img src="Images/Empresa/Galeria/3.webp">
+                        </div>
+                        <div id="4">
+                            <img src="Images/Empresa/Galeria/4.webp">
+                        </div>
+                        <div id="5">
+                            <img src="Images/Empresa/Galeria/5.webp">
+                        </div>
+                        <div id="6">
+                            <img src="Images/Empresa/Galeria/6.webp">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <?php
+        include 'php/form.html';
+        include 'php/maps.html';
+        include 'php/footer.html';
+        ?>
+
+    </body>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+</html>
